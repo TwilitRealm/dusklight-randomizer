ea771b2ae4f02e120214b9667d256ceb2dda3b5b
gymnast86 <stock.joann@gmail.com>
2026-08-02 01:48:58 -0700

    add logic tests executable

--- generator/test/test.cpp
+++ generator/test/test.cpp
@@ -1,42 +1,39 @@
-#include "test.hpp"
-
 #include "../randomizer.hpp"
 #include "../utility/string.hpp"
 
 #include <filesystem>
 #include <iostream>
 
-namespace randomizer::test::test
+int main()
 {
-    void RunTests()
+    for (const auto& entry : std::filesystem::recursive_directory_iterator(RANDO_LOGIC_TESTS_PATH))
     {
-        for (const auto& entry : std::filesystem::recursive_directory_iterator(RANDO_LOGIC_TESTS_PATH))
+        if (entry.path().generic_string().ends_with("settings.yaml"))
         {
-            if (entry.path().generic_string().ends_with("settings.yaml"))
-            {
-                auto pathFolders = utility::str::Split(entry.path().generic_string(), '/');
-                auto& testName = pathFolders[pathFolders.size() - 2];
-                std::filesystem::remove(SETTINGS_PATH);
-                std::filesystem::copy_file(entry, SETTINGS_PATH);
+            auto pathFolders = randomizer::utility::str::Split(entry.path().generic_string(), '/');
+            auto& testName = pathFolders[pathFolders.size() - 2];
+            std::filesystem::remove(SETTINGS_PATH);
+            std::filesystem::copy_file(entry, SETTINGS_PATH);
 
-                std::cout << "Testing " << testName << std::endl;
+            std::cout << "Testing " << testName << std::endl;
 
-                try {
-                    Randomizer r{RANDO_SAVE_PATH};
-                    r.GenerateWorlds();
-                }
-                catch(const std::exception& e) {
-                    std::cout << "Test \"" << testName << "\" failed! Failed settings saved to " << SETTINGS_PATH << std::endl;
-                    std::cout << "Error Message: " << e.what() << std::endl;
-                    throw;
-                }
-
-                std::filesystem::remove(SETTINGS_PATH);
+            try {
+                randomizer::Randomizer r{RANDO_SAVE_PATH};
+                r.GenerateWorlds();
             }
-        }
-        // Remove test preferences
-        std::filesystem::remove(PREFERENCES_PATH);
+            catch(const std::exception& e) {
+                std::cout << "Test \"" << testName << "\" failed! Failed settings saved to " << SETTINGS_PATH << std::endl;
+                std::cout << "Error Message: " << e.what() << std::endl;
+                return 1;
+            }
 
-        std::cout << "All Settings Tests passed" << std::endl;
+            std::filesystem::remove(SETTINGS_PATH);
+        }
     }
-} // namespace randomizer::test::test
+    // Remove test preferences
+    std::filesystem::remove(PREFERENCES_PATH);
+
+    std::cout << "All Settings Tests passed" << std::endl;
+
+    return 0;
+}
--- generator/test/test.hpp
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-namespace randomizer::test::test
-{
-    void RunTests();
-} // namespace randomizer::test::test
